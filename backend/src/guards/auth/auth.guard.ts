f2e192e1e5530a4af59f503c88a9fefa49f18189
codegatty <rajithgatty61@gmail.com>
2025-01-21 22:19:24 +0530

    --added aunthentication using http only cookie with access token and refresh token

---
+++ backend/src/guards/auth/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new HttpException('Invalid access token', HttpStatus.UNAUTHORIZED);
+    }
+    try {
+      const payload = this.jwtService.verify(token);
+      request['userId'] = payload.userId;
+    } catch {
+      throw new HttpException('Invalid access token', HttpStatus.UNAUTHORIZED);
+    }
+    return true;
+  }
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
