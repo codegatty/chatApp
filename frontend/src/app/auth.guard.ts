45117cf694f2fb66ff9bc24599ad10a87ca52db5
codegatty <rajithgatty61@gmail.com>
2025-01-27 12:53:45 +0530

    --added persistent login

--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -1,17 +1,24 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { ICurrentUser } from './interface/current_user';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
-export const authGuard: CanActivateFn =async (route, state) => {
-  const authService=inject(AuthService)
-  const router=inject(Router)
-console.log("interfiered by Guard")
-  if(authService.getAccessToken()){
-  return true;
-  
-  }else{
-    // const a= authService.refresh()
-   router.navigate(['/login'])
-    return false
-  }
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  console.log("Interfered by Guard");
+
+  return authService.refresh().pipe(
+    switchMap((user) => {
+      authService.setCurrentUser(user as ICurrentUser);
+      return of(true); // Allow navigation
+    }),
+    catchError(() => {
+      router.navigate(['/login']);
+      return of(false); // Block navigation
+    })
+  );
 };
